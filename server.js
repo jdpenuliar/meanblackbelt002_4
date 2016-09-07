6fc7ce767c78a6924b8daa3396a0bab325b1dd86
jdpenuliar <jaydz.penuliar@gmail.com>
2016-09-06 21:17:19 -0700

    add index.html in client dir and basic server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,"./client")));
+
+app.listen(8000,function(){
+	console.log("haha on 8000");
+});
